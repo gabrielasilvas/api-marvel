3fe614d144088f7f0081debdbd2556e378b9afe5
gabrielasilvas <86064082+gabrielasilvas@users.noreply.github.com>
2021-06-17 18:32:19 -0300

    Delete Downloads/nathan/quadrinhos-app directory

--- Downloads/nathan/quadrinhos-app/src/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('Deve desenhar a lista não ordenada de comics', () => {
-  render(<App />);
-  const listaNaoOrdenada = screen.getByTestId('lista-comics');
-  expect(listaNaoOrdenada).toBeInTheDocument();
-});
